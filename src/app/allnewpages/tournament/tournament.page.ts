a707f8ca6b5b779efd7bef70f21a99577ed359a9
Nathanael <50619296+NathanaelZondo@users.noreply.github.com>
2019-11-25 17:36:50 +0200

    ui

--- src/app/allnewpages/tournament/tournament.page.ts
+++ src/app/allnewpages/tournament/tournament.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-tournament',
   templateUrl: './tournament.page.html',
@@ -8,8 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class TournamentPage implements OnInit {
 
   constructor() { }
-
+fixture =[];
+obj:any ={};
   ngOnInit() {
+
+    firebase.firestore().collection('MatchFixtures').get().then(res=>{
+      res.forEach(val=>{
+       
+
+        let ndate = new Date(val.data().matchdate);
+        
+        this.obj =val.data();
+        this.obj.matchtime =ndate.toLocaleTimeString();
+
+
+
+        this.obj.matchdate=' '+ndate.toLocaleDateString();
+        console.log(this.obj.matchdate)
+        console.log("Time = ",ndate.toLocaleDateString())
+        console.log({...this.obj,...{id:val.id}})
+        this.fixture.push({...this.obj,...{id:val.id}})
+      })
+    })
   }
+
+  currentmatch(item)
+  {
+console.log(item)
+
+  }
+
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase'
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { config } from '../app/FirebaseConfig';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -15,7 +15,9 @@ export class AppComponent {
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+    firebase.initializeApp(config)
     this.initializeApp();
+
   }
 
   initializeApp() {
