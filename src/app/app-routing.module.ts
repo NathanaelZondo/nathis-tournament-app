9e341fdefcdb73d671f1ae25714d202c62b95710
Nathanael <50619296+NathanaelZondo@users.noreply.github.com>
2019-11-25 15:07:32 +0200

    little stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'tournament',
+    loadChildren: () => import('./allnewpages/tournament/tournament.module').then( m => m.TournamentPageModule)
+  },
 ];
 
 @NgModule({
