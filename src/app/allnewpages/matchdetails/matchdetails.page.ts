ce8bffd340a568cd4149573446a04b743a87d50e
Keketso Matsuma <49682202+MatsumaKeketso@users.noreply.github.com>
2019-12-05 08:31:18 +0200

    gg

--- src/app/allnewpages/matchdetails/matchdetails.page.ts
+++ src/app/allnewpages/matchdetails/matchdetails.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AllserveService } from 'src/app/allservices/allserve.service';
 import * as firebase from 'firebase';
-import { Subscription, Observable, observable,timer } from 'rxjs';
+import { Subscription, Observable, observable, timer } from 'rxjs';
 import { LoadingController, IonicModule, Platform } from '@ionic/angular';
 @Component({
   selector: 'app-matchdetails',
@@ -9,53 +9,29 @@ import { LoadingController, IonicModule, Platform } from '@ionic/angular';
   styleUrls: ['./matchdetails.page.scss'],
 })
 export class MatchdetailsPage implements OnInit {
-  currentmatch =this.allserve.currentmatch;
-  sub :Subscription;
+  currentmatch = this.allserve.currentmatch;
+  sub: Subscription;
   timer;
-  cmatch =[];
-  constructor(public plt:Platform,public allserve:AllserveService ) {
+  cmatch = [];
+  constructor(public plt: Platform, public allserve: AllserveService) {
+    this.cmatch.push(this.currentmatch);
+    console.log(this.cmatch)
 
-  
-this.cmatch.push(this.currentmatch);
-console.log(this.cmatch)
-
-    this.plt.ready().then((readySource) =>{
+    this.plt.ready().then((readySource) => {
       console.log('Platform ready from', readySource);
-      this.sub = timer(0,19000).subscribe(result =>{
-       
+      this.sub = timer(0, 19000).subscribe(result => {
 
+        console.log('docid = ', this.currentmatch.id)
 
-
-
-
-
-console.log('docid = ',this.currentmatch.id)
-
-        firebase.firestore().collection('MatchFixtures').doc(this.currentmatch.id).onSnapshot(res=>{
+        firebase.firestore().collection('MatchFixtures').doc(this.currentmatch.id).onSnapshot(res => {
           // console.log(res.data())
-          this.timer =res.data().timer;
+          this.timer = res.data().timer;
         })
-        
-        
-    
-    
-        
-
-
-
-
-
-
-
-
-
       }
-      )})
+      )
+    })
 
-
-
-
-   }
+  }
 
   ngOnInit() {
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  {
-    path: 'tournament',
-    loadChildren: () => import('./allnewpages/tournament/tournament.module').then( m => m.TournamentPageModule)
-  },
-  {
-    path: 'matchdetails',
-    loadChildren: () => import('./allnewpages/matchdetails/matchdetails.module').then( m => m.MatchdetailsPageModule)
-  },
+  { path: 'tournament', loadChildren: () => import('./allnewpages/tournament/tournament.module').then( m => m.TournamentPageModule)},
+  { path: 'matchdetails', loadChildren: () => import('./allnewpages/matchdetails/matchdetails.module').then( m => m.MatchdetailsPageModule)},
 ];
 
 @NgModule({
