a2b6f3aca1c8e9a387685074c5f14e5deee6bddb
NathanaelZondo <fumanizondo@gmail.com>
2020-07-02 14:59:10 +0200

    Update FirebaseConfig.ts

--- src/app/FirebaseConfig.ts
+++ src/app/FirebaseConfig.ts
@@ -1,11 +1,11 @@
 export const config = {
 
-    apiKey: "AIzaSyCqgZTC37DgUXU7MWeIaNXP7TSTP0SkI2o",
-  authDomain: "nathistournamentdb.firebaseapp.com",
-  databaseURL: "https://nathistournamentdb.firebaseio.com",
-  projectId: "nathistournamentdb",
-  storageBucket: "nathistournamentdb.appspot.com",
-  messagingSenderId: "743242408134",
-  appId: "1:743242408134:web:47eaf5dabebe24dc90f7c7",
-  measurementId: "G-3VQ90REKVP"
+  apiKey: "AIzaSyDmAaA9cu3wT3J9VS9tmdAD4evqMMw6EWM",
+  authDomain: "nathi-stourmentdb.firebaseapp.com",
+  databaseURL: "https://nathi-stourmentdb.firebaseio.com",
+  projectId: "nathi-stourmentdb",
+  storageBucket: "nathi-stourmentdb.appspot.com",
+  messagingSenderId: "564444309839",
+  appId: "1:564444309839:web:e0dd30714011c17a8bbd3a",
+  measurementId: "G-L2DZXGMXKP"
 }
